ad228ea02fd2f4704d7bb1bfc295ea76959ab75e
Dan Mindru <mindrudan@gmail.com>
2014-10-25 18:49:57 +0200

    removed unused themes, switched from SASS to less

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,18 +37,11 @@ module.exports = function(grunt) {
 			}
 		},
 
-		sass: {
+		less: {
 			main: {
 				files: {
-					'css/theme/default.css': 'css/theme/source/default.scss',
-					'css/theme/beige.css': 'css/theme/source/beige.scss',
-					'css/theme/night.css': 'css/theme/source/night.scss',
-					'css/theme/serif.css': 'css/theme/source/serif.scss',
-					'css/theme/simple.css': 'css/theme/source/simple.scss',
-					'css/theme/sky.css': 'css/theme/source/sky.scss',
-					'css/theme/moon.css': 'css/theme/source/moon.scss',
-					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/default.css': 'css/theme/source/default.less',
+					'css/theme/solarized.css': 'css/theme/source/solarized.less'
 				}
 			}
 		},
@@ -102,7 +95,7 @@ module.exports = function(grunt) {
 				tasks: 'default'
 			},
 			theme: {
-				files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+				files: [ 'css/theme/source/*.less', 'css/theme/template/*.less' ],
 				tasks: 'themes'
 			}
 		}
@@ -115,7 +108,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
 	grunt.loadNpmTasks( 'grunt-zip' );
 
@@ -123,7 +116,7 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'default', [ 'jshint', 'cssmin', 'uglify', 'qunit' ] );
 
 	// Theme task
-	grunt.registerTask( 'themes', [ 'sass' ] );
+	grunt.registerTask( 'themes', [ 'less' ] );
 
 	// Package presentation to archive
 	grunt.registerTask( 'package', [ 'default', 'zip' ] );
